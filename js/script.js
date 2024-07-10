6c1adf0897e3f2d653838ca5562c159fbf1453eb
pedro-klein-desco <pedro.klein@descomplica.com.br>
2024-07-09 22:47:15 -0300

    :sparkles: feat: loading bar and table show results

--- js/script.js
+++ js/script.js
@@ -5,11 +5,12 @@ const paramA = document.getElementById('paramA');
 const paramB = document.getElementById('paramB');
 const paramC = document.getElementById('paramC');
 const valorFinal = document.getElementById('valor_final');
+const loading = document.getElementById('loading');
 
 function calcularY(x, a, b, c) {
     const expPart = Math.exp(a * (x - b));
     const y = c + ((1 - c) / (1 + expPart));
-    return y.toFixed(2);  // Arredonda para 2 casas decimais
+    return y.toFixed(2); 
 }
 
 form.addEventListener('submit', function(event) {
@@ -17,25 +18,34 @@ form.addEventListener('submit', function(event) {
 
     const apiUrl = 'http://localhost:8000/sintetize/' + userText.value;
 
+    loading.style.display = 'block';
+    resultArea.style.display = 'none';
+
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
             console.log('Dados recebidos:', data);
             const { a, b, c } = data;
 
-            const resultsA = `Parâmetro A: ${a}`;
-            const resultsB = `Parâmetro B: ${b}`;
-            const resultsC = `Parâmetro C: ${c}`;
+            const resultsA = `${a.toFixed(2)}`;
+            const resultsB = `${b.toFixed(2)}`;
+            const resultsC = `${c.toFixed(2)}`;
 
             const resultsValorFinal = calcularY(0.7, a, b, c);
 
             paramA.textContent = resultsA;
             paramB.textContent = resultsB;
             paramC.textContent = resultsC;
-            valorFinal.textContent = `TRI: ${resultsValorFinal}`;
+            valorFinal.textContent = `${resultsValorFinal}`;
+
+            // Esconder a barra de carregamento e mostrar o resultado
+            loading.style.display = 'none';
             resultArea.style.display = 'block';
         })
         .catch(error => {
             console.error('Erro ao fazer requisição:', error);
+
+            // Esconder a barra de carregamento em caso de erro
+            loading.style.display = 'none';
         });
 });
