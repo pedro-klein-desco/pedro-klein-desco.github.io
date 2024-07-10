512cf9530e6f60ccadce742deea16824ca9d8499
Pedro Falcão <132491358+pedrofalcaodesco@users.noreply.github.com>
2024-07-10 12:35:59 -0300

    :hammer: fix: correct response text

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const valorFinal = document.getElementById('valor_final');
 const loading = document.getElementById('loading');
 const loadingBar = document.getElementById('loading-bar');
 
-function calcularY(x, a, b, c) {
+function calcularY(a, b, c) {
     const logPart = Math.log((1 - c)/(0.51 - c) - 1);
     const y = 100*((1/a)*logPart + b);
     return y.toFixed(2); 
@@ -46,12 +46,12 @@ form.addEventListener('submit', function(event) {
             const resultsB = `${b.toFixed(2)}`;
             const resultsC = `${c.toFixed(2)}`;
 
-            const resultsValorFinal = calcularY(0.4 , a, b, c);
+            const resultsValorFinal = calcularY(a, b, c);
 
             paramA.textContent = resultsA;
             paramB.textContent = resultsB;
             paramC.textContent = resultsC;
-            valorFinal.textContent = `${resultsValorFinal}`;
+            valorFinal.textContent = `Pela análise do enunciado, espera-se que os alunos à partir do nível de proficiência ${resultsValorFinal} sejam capazes de responder a questão corretamente. Isso significa que, se você a acertou, você provavelmente está nesse nível.`;
 
             loading.style.display = 'none';
             resultArea.style.display = 'block';
